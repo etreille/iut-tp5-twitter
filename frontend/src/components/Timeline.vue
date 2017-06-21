7f248e931c46b0ac9242470f07738eab6bc23f48
etreille <tonashss@hotmail.fr>
2017-06-21 11:50:58 +0200

    list of tweets with author and content

--- frontend/src/components/Timeline.vue
+++ frontend/src/components/Timeline.vue
@@ -1,10 +1,8 @@
 <template>
   <div class="timeline">
     <ul>
-        <li v-for="tweet in tweets">
-              {{ tweet }}
-            </li>
-
+      <li v-for="tweet in tweets">
+        {{ tweet.auteur }} : {{tweet.contenu}}
       </li>
     </ul>
   </div>
@@ -15,8 +13,22 @@ export default {
   name: 'hello',
   data () {
     return {
-      tweets: ['tweet 1', 'tweet 2', 'tweet 3']
+      tweets: [
+        {auteur: 'Jean', contenu: 'Hello Tweeter!'},
+        {auteur: 'Bob', contenu: 'tweet 3'}
+      ]
     }
   }
 }
 </script>
+
+<style scoped>
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: block;
+    margin: 0 10px;
+  }
+</style>
