be52f1b4b12a5b4eaf3f53b346dfb8d084999d33
etreille <tonashss@hotmail.fr>
2017-06-21 11:35:52 +0200

    Add Timeline component

---
+++ frontend/src/components/Timeline.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="timeline">
+    <ul>
+        <li v-for="tweet in tweets">
+              {{ tweet }}
+            </li>
+
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      tweets: ['tweet 1', 'tweet 2', 'tweet 3']
+    }
+  }
+}
+</script>
